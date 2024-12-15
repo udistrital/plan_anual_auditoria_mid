0a877954f34a68f998c418def8e67ea627f27428
Juan Castro <juan941223@gmail.com>
2024-12-14 23:04:59 -0500

    feat: cargue masivo actividades udistrital/sisifo_documentacion#317

--- src/cargue-masivo/cargue-masivo.controller.ts
+++ src/cargue-masivo/cargue-masivo.controller.ts
@@ -7,11 +7,12 @@ import {
 } from '@nestjs/common';
 import { CargueMasivoService } from './cargue-masivo.service';
 import axios from 'axios';
+import { environment } from 'src/config/configuration';
 
 @Controller('cargue-masivo')
 export class CargueMasivoController {
   constructor(private readonly cargueMasivoService: CargueMasivoService) {}
-
+  private serverlessUrl = environment.CARGUE_MASIVO_SERVERLESS_MID;
   @Post('auditorias')
   async cargueMasivo(@Body() payload: any): Promise<any> {
     try {
@@ -23,8 +24,39 @@ export class CargueMasivoController {
         type_upload,
       );
 
-      const serverlessUrl = process.env.CARGUE_MASIVO_SERVERLESS_MID;
-      const response = await axios.post(serverlessUrl, estructura);
+     // const serverlessUrl = environment.CARGUE_MASIVO_SERVERLESS_MID;
+      const response = await axios.post(this.serverlessUrl, estructura);
+
+      return response.data;
+    } catch (error) {
+      console.error('Error procesando la solicitud:', error);
+
+      if (axios.isAxiosError(error) && error.response) {
+        throw new HttpException(
+          `Error en el serverless: ${error.response.data.message || error.response.statusText}`,
+          error.response.status,
+        );
+      }
+
+      throw new HttpException(
+        'Error procesando la solicitud',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  
+  @Post('actividades')
+  async cargueMasivoActividades(@Body() payload: any): Promise<any> {
+    try {
+      const { base64data, complement, type_upload } = payload;
+
+      const estructura = await this.cargueMasivoService.crearEstructuraActividad(base64data,complement,type_upload,);
+      console.log("url: ", this.serverlessUrl)
+      console.log("estructura: ", estructura)
+
+      //const serverlessUrl = environment.CARGUE_MASIVO_SERVERLESS_MID;
+      const response = await axios.post(this.serverlessUrl, estructura);
+
       return response.data;
     } catch (error) {
       console.error('Error procesando la solicitud:', error);
@@ -43,3 +75,4 @@ export class CargueMasivoController {
     }
   }
 }
+
