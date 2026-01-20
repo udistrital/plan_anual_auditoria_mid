bc0ae1dad748ebd4eb2c2bd252f05adabf8079fb
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2026-01-20 18:02:52 -0500

    feat: udistrital/sisifo_documentacion#443	FORMATO_Y_ESTRUCTURA_DE_DATOS
    - Se desarrolla la última parte relacionada a la organización y formato de datos.

--- src/plantilla/services/plantilla-programa-trabajo.service.ts
+++ src/plantilla/services/plantilla-programa-trabajo.service.ts
@@ -1,14 +1,18 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as moment from 'moment';
 import 'moment/locale/es';
 import { PlantillaUtilsService } from '../../utils/plantilla.utils';
 import { environment } from 'src/config/configuration';
 import { lastValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
+import { capitalize, unirListaNombres } from 'src/utils/texto.utils';
 
 
 const {
     PLAN_AUDITORIA_CRUD_SERVICE,
     PLANTILLAS,
+    PARAMETROS_SERVICE,
+    TERCEROS_SERVICE,
 } = environment;
 
 @Injectable()
@@ -20,8 +24,52 @@ export class PlantillaProgramaTrabajoService {
 
     async get(idAuditoria: string) {
         const auditoria = await this.plantillaUtils.obtenerAuditoria(idAuditoria);
-        const actividades = await this.obtenerActividades(idAuditoria);
-        return 0;
+        const infoParaPlantilla = await this.organizarData(auditoria);
+        const baseRenderizado = await this.plantillaUtils.renderizarPlantilla(infoParaPlantilla);
+        return baseRenderizado;
+    }
+
+    private async organizarData(data: any) {
+        try {
+            const auditoria = data.auditoria;
+            const [actividades, macroproceso, cargoLider, cargoResponsable, grupoAuditor] = await Promise.all([
+            this.obtenerActividades(auditoria._id),
+            this.traerParametros(auditoria.macroproceso),
+            this.traerParametros(auditoria.lider_id),
+            this.traerParametros(auditoria.responsable_id),
+            this.obtenerNombresAuditores(auditoria._id)
+        ]);
+
+        const infoParaPlantilla = {
+            plantilla_id: PLANTILLAS.PROGRAMA_TRABAJO,
+            data: {
+                items: actividades.map((dataActividad) => ({
+                    actividad: dataActividad.titulo,
+                    auditor: grupoAuditor,
+                    fechaInicial: moment(dataActividad.fecha_inicio).format('YYYY-MM-DD'),
+                    fechaFinal: moment(dataActividad.fecha_fin).format('YYYY-MM-DD')
+                })),
+                recursosTecnologicos: auditoria.rec_tecnologico,
+                recursosHumanos: auditoria.rec_humano,
+                recursosMateriales: auditoria.rec_fisico,
+                macroproceso: macroproceso.Nombre,
+                lider: cargoLider.Nombre,
+                responsable: cargoResponsable.Nombre,
+                objetivos: auditoria.objetivo,
+                alcance: auditoria.alcance,
+                criterios: auditoria.criterio,
+                grupoAuditor: grupoAuditor,
+                fechaEjecucion: moment(auditoria.fecha_inicio).format('DD/MM/YYYY'),
+            }
+        };
+
+        return infoParaPlantilla;
+        } catch (error) {
+            throw new HttpException(
+                'Error al organizar los datos para la plantilla',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
     }
 
     private async obtenerActividades(idAuditoria: string) {
@@ -35,6 +83,77 @@ export class PlantillaProgramaTrabajoService {
             throw new HttpException(
                 'Error al obtener los datos del servicio externo ',
                 error,
+            );
+        }
+    }
+
+    private async traerParametros(idParam: string) {
+        const url = `${PARAMETROS_SERVICE}/parametro?query=Id:${idParam}&fields=Nombre`;
+        try {
+            const response = await lastValueFrom(this.httpService.get(url));
+            return response.data.Data[0];
+        } catch (error) {
+            throw new HttpException(
+                'Error al obtener los datos del servicio externo',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    private async obtenerNombresAuditores(auditoriaId: string): Promise<string> {
+        const auditores = await this.traerAuditores(auditoriaId);
+
+        if (!auditores?.length) return 'No se encontraron auditores.';
+
+        const nombres = await Promise.all(
+            auditores.map(async (auditor) => {
+                try {
+                    const tercero = await this.traerTercero(auditor.auditor_id);
+                    return tercero?.NombreCompleto
+                        ? capitalize(tercero.NombreCompleto)
+                        : null;
+                } catch (error) {
+                    console.error(
+                        `Error al obtener tercero ${auditor.auditor_id}:`,
+                        error,
+                    );
+                    return null;
+                }
+            }),
+        );
+        
+
+        const todosValidos = nombres.every((nombre) => nombre !== null);
+
+        if (!todosValidos) {
+            return 'Error al obtener los nombres de los auditores';
+        }
+
+        return unirListaNombres(nombres);
+    }
+
+    private async traerAuditores(auditoriaId: string) {
+        const url = `${PLAN_AUDITORIA_CRUD_SERVICE}auditor?query=auditoria_id:${auditoriaId},activo:true&limit=0&fields=auditor_id`;
+        try {
+            const response = await lastValueFrom(this.httpService.get(url));
+            return response.data.Data;
+        } catch (error) {
+            throw new HttpException(
+                'Error al obtener los datos de terceros',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+    private async traerTercero(terceroId: string) {
+        const url = `${TERCEROS_SERVICE}tercero/${terceroId}`;
+        try {
+            const response = await lastValueFrom(this.httpService.get(url));
+            return response.data;
+        } catch (error) {
+            throw new HttpException(
+                'Error al obtener los datos de terceros',
+                HttpStatus.INTERNAL_SERVER_ERROR,
             );
         }
     }
