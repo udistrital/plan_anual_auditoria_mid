a43f77a8de2f5d2e18a87224793d7391d84de1dd
MrPocketMonsters <ndsabogalv@gmail.com>
2026-02-19 11:20:35 -0500

    feat: Añadir renderizado de macroproceso, proceso y dependencia a pdf de PAA en endpoint plantilla udistrital/sisifo_documentacion#493

--- src/plantilla/services/plantilla.service.ts
+++ src/plantilla/services/plantilla.service.ts
@@ -9,7 +9,15 @@ const {
   PLANTILLAS,
   MESES,
   PLANTILLAS_MID_SERVICE,
+  PARAMETROS_SERVICE,
+  OIKOS_SERVICE,
 } = environment;
+
+/** Interface representing a parameter with an ID and a name. */
+interface Parametro {
+  Id: number;
+  Nombre: string;
+}
 
 @Injectable()
 export class PlantillaService {
@@ -26,7 +34,7 @@ export class PlantillaService {
 
   private async traerDataCrud(id: string) {
     let urlPlanAuditoria = `${PLAN_AUDITORIA_CRUD_SERVICE}plan-auditoria/${id}`;
-    let urlAuditioria = `${PLAN_AUDITORIA_CRUD_SERVICE}auditoria?query=plan_auditoria_id:${id},activo:true&fields=titulo,cronograma_id&limit=0`;
+    let urlAuditioria = `${PLAN_AUDITORIA_CRUD_SERVICE}auditoria?query=plan_auditoria_id:${id},activo:true&fields=titulo,macroproceso_id,proceso_id,dependencia_id,cronograma_id&limit=0`;
     try {
       const responsePlanAuditoria = await lastValueFrom(
         this.httpService.get(urlPlanAuditoria),
@@ -53,7 +61,7 @@ export class PlantillaService {
    */
   private async anadirDataEspeciales(data: any) {
     const vigenciaId = data.dataPlanAuditoria.Data?.vigencia_id;
-    const urlAuditioria = `${PLAN_AUDITORIA_CRUD_SERVICE}auditoria?query=vigencia_id:${vigenciaId},plan_anual_auditoria_id__is_null:true,activo:true&fields=titulo,cronograma_id&limit=0`;
+    const urlAuditioria = `${PLAN_AUDITORIA_CRUD_SERVICE}auditoria?query=vigencia_id:${vigenciaId},plan_anual_auditoria_id__is_null:true,activo:true&fields=titulo,macroproceso_id,proceso_id,dependencia_id,cronograma_id&limit=0`;
 
     try {
       const responseAuditoriaEspecial = await lastValueFrom(
@@ -80,11 +88,11 @@ export class PlantillaService {
       auditoriasOrden,
     );
 
-    const items: PlantillaDto[] = Array.isArray(auditorias)
-      ? auditoriasOrdenadas.map((auditoria: any) =>
+    const items: Promise<PlantillaDto[]> = Array.isArray(auditorias)
+      ? Promise.all(auditoriasOrdenadas.map((auditoria: any) =>
           this.organizarItems(auditoria),
-        )
-      : [];
+        ))
+      : Promise.resolve([]);
 
     const auditoriasEspeciales = data.dataAuditoriaEspecial?.Data || [];
     const auditoriasEspecialesOrden = data.dataPlanAuditoria?.Data?.auditorias_especiales || [];
@@ -93,11 +101,11 @@ export class PlantillaService {
       auditoriasEspecialesOrden,
     );
 
-    const especiales: PlantillaDto[] = Array.isArray(auditoriasEspeciales)
-      ? auditoriasEspecialesOrdenadas.map((auditoria: any) =>
+    const especiales: Promise<PlantillaDto[]> = Array.isArray(auditoriasEspeciales)
+      ? Promise.all(auditoriasEspecialesOrdenadas.map((auditoria: any) =>
           this.organizarItems(auditoria),
-        )
-      : [];
+        ))
+      : Promise.resolve([]);
 
     json.plantilla_id = PLANTILLAS.PLAN_ANUAL_AUDITORIA;
     json.data = {
@@ -107,13 +115,15 @@ export class PlantillaService {
       alcance: data.dataPlanAuditoria.Data?.alcance || '',
       criterios: data.dataPlanAuditoria.Data?.criterio || '',
       recursos: data.dataPlanAuditoria.Data?.recurso || '',
-      items: items,
-      especiales: especiales,
+      items: await items,
+      especiales: await especiales,
     };
 
+    console.debug('JSON generado para renderizar:', JSON.stringify(json, null, 2));
     return json;
   }
-  private organizarItems(data: any): PlantillaDto {
+
+  private async organizarItems(data: any): Promise<PlantillaDto> {
     const idMesMap = {
       [MESES.ENERO]: 'enero',
       [MESES.FEBRERO]: 'febrero',
@@ -146,8 +156,41 @@ export class PlantillaService {
       }
     });
 
+    const macroproceso = data.macroproceso_id == null
+                          ? 'No definido'
+                          : await this.getParametroName(data.macroproceso_id)
+                              .catch(error => {
+                                console.error(
+                                  `Error fetching macroproceso name for ID ${data.macroproceso_id}:`, error
+                                );
+                                return '?error';
+                              });
+
+    const proceso = data.proceso_id == null
+                    ? 'No definido'
+                    : await this.getParametroName(data.proceso_id)
+                        .catch(error => {
+                          console.error(
+                            `Error fetching proceso name for ID ${data.proceso_id}:`, error
+                          );
+                          return '?error';
+                        });
+
+    const dependencia = data.dependencia_id == null
+                      ? 'No definido'
+                      : await this.getDependenciaName(data.dependencia_id)
+                          .catch(error => {
+                            console.error(
+                              `Error fetching dependencia name for ID ${data.dependencia_id}:`, error
+                            );
+                            return '?error';
+                          });
+
     return {
-      actividad: data.titulo || '',
+      actividad: data.titulo || 'No definido',
+      macroproceso: macroproceso,
+      proceso: proceso,
+      dependencia: dependencia,
       ...mesesMarcados,
     };
   }
@@ -180,6 +223,83 @@ export class PlantillaService {
     } catch (error) {
       console.error('Error al enviar data:', error);
       throw new Error('No se pudo enviar el plan');
+    }
+  }
+
+    /**
+   * Obtains a parametro's name given its id by fetching data from the PARAMETROS_SERVICE.
+   * @param parametroId The ID of the parameter fetch.
+   * @returns A promise that resolves to the name of the parameter corresponding to the given ID.
+   * @throws An error if the fetch operation fails or if the response is not in the expected format.
+   */
+  private async getParametroName(parametroId: number): Promise<string> {
+    const url = `${PARAMETROS_SERVICE}parametro?query=Activo:true,Id:${parametroId}&fields=Id,Nombre&limit=0`;
+    console.debug(`Fetching [${parametroId}] type parameters from URL: ${url}`);
+    try {
+      const name = await this.fetchParametroName(url);
+      console.debug(`Fetched parameter name: ${name}`);
+      return name;
+    }
+    catch (error) {
+      const newError = new Error('Failed to get parametro');
+      newError.stack += "\nCaused by: " + error.stack;
+      throw newError;
+    }
+  }
+
+  /**
+   * Obtains a dependency's name given its id by fetching data from the OIKOS_SERVICE.
+   * @returns A promise that resolves to the name of the dependency corresponding to the given ID.
+   * @throws An error if the fetch operation fails or if the response is not in the expected format.
+   */
+  private async getDependenciaName(dependenciaId: number): Promise<string> {
+    const url = `${OIKOS_SERVICE}dependencia?query=Activo:true,Id:${dependenciaId}&fields=Id,Nombre&limit=0`;
+    console.debug(`Fetching dependencia name from URL: ${url}`);
+    try {
+      const name = await this.fetchParametroName(url);
+      console.debug(`Fetched dependencia name: ${name}`);
+      return name;
+    }
+    catch (error) {
+      const newError = new Error('Failed to get dependencias');
+      newError.stack += "\nCaused by: " + error.stack;
+      throw newError;
+    }
+  }
+
+  /**
+   * Fetches parameters from a given URL and returns a mapping of parameter names to their corresponding IDs.
+   * @param url The URL to fetch the parameters from.
+   * @returns A promise that resolves to a mapping of parameter names to IDs.
+   * @throws An error if the fetch operation fails or if the response is not in the expected format (See {@link Parametro}).
+   */
+  private async fetchParametroName(url: string): Promise<string> {
+    try {
+      // Fetch the parameters from the given URL
+      console.debug(`Fetching parameters from URL: ${url}`);
+      const response = await lastValueFrom(this.httpService.get(url));
+      const axiosData = response.data;
+      const data: Parametro[] = axiosData.Data || axiosData;
+
+      // Validate the response format
+      if (!Array.isArray(data))
+        throw new Error('Invalid response format: expected an array');
+      
+      if (!data.every(item => 'Id' in item && 'Nombre' in item))
+        throw new Error('Invalid response format: expected objects with Id and Nombre properties');
+
+      // Validate that we have at least one parameter in the response
+      if (data.length === 0)
+        throw new Error('No parameters found in the response');
+
+      // Assume the first parameter in the response is the one we want (since we query by ID, there should be at most one)
+      const parametroName = data[0].Nombre;
+      return parametroName;
+    }
+    catch (error) {
+      const newError = new Error(`Failed to fetch parameters from ${url}`);
+      newError.stack += "\nCaused by: " + error.stack;
+      throw newError;
     }
   }
 }
