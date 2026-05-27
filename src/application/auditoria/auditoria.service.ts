5aa760c7e82c2187ea92626bb0dfad95f4778348
jupmorenor <jupmorenor@gmail.com>
2026-05-27 14:21:18 -0500

    fix: Se ajusta obtención asincrona de datos de dependencias. udistrital/sisifo_documentacion#830

--- src/application/auditoria/auditoria.service.ts
+++ src/application/auditoria/auditoria.service.ts
@@ -617,12 +617,12 @@ export class AuditoriaService {
     if ('dependencia_id' in firstElement) {
       observables['dependencia_id'] = this.dominiosService.getDependencias();
       if (!Array.isArray(data.Data)) {
-        this.asegurarArray(data.Data.dependencia_id)
+        await Promise.all(this.asegurarArray(data.Data.dependencia_id)
           .filter((dep_id) => dep_id != null)
-          .forEach(async (dep_id) => {
+          .map(async (dep_id) => {
             const datos = await this.getDatosTerceros(dep_id);
             this.datosTerceros.push(datos);
-          });
+          }));
       }
     }
 
